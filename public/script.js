3bdc7af906ce2bbbae9bd59fac54f9c10ba0278c
nv.duenas <nv.duenas@uniandes.edu.co>
2021-10-01 14:37:31 -0500

    initial

--- public/script.js
+++ public/script.js
@@ -7,8 +7,8 @@ const message = document.getElementById('message');
 
 enterButton.addEventListener('click', (event) => {
   //Implementar lógica del button submit
-  alert('Implementar lógica del button submit');
-  getresults(123);
+  height = input.value;
+  getresults(height);
   event.preventDefault();
 });
 
@@ -20,7 +20,26 @@ async function getresults(heightRef) {
   const resp = await fetch(`api?input=${heightRef}`);
   const data = await resp.json();
   console.log('data from back', data);
-  //printValues(data);
+  if(data.length > 0){
+    printValues(data);
+  }
+  else{
+    message.innerText = "No matches found";
+  }
+}
+
+function printValues(data){
+  data.forEach((el, index) =>{
+      console.log(el)
+      let row = table.insertRow(index);
+      let id = row.insertCell(0)
+      let first_player = row.insertCell(1);
+      let second_player = row.insertCell(2);
+
+      id.innerHTML = index+1
+      first_player.innerHTML = el.first_player;
+      second_player.innerHTML = el.second_player;
+  });
 }
 
 function removeAllChildNodes(parent) {
--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,29 @@
-const { response, request } = require('express');
-
+const { response, request } = require("express");
+const axios = require("axios");
+const map = new Map();
+axios
+  .get(
+    "https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players"
+  )
+  .then((response) => {
+    players = response.data.values;
+    players.forEach((element) => {
+      map.set(element.first_name + element.last_name, element.h_meters * 39.37);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const getPairsOfPlayers = async (req = request, resp = response, next) => {
   //Implementar logica aquí
-  return resp.json({ message: 'To be implemented' });
+  let input = request.query.input;
+  let res = [];
+  map.forEach((value, key) => {
+    if (map.has(Math.abs(input - value))) {
+      res.push({ first_player: key, second_player: map.get(input - value) });
+    }
+  });
+  return resp.json(res);
 };
 
 module.exports = { getPairsOfPlayers };
